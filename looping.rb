a604359e68302f00aaf084e6cfc6eee0fb0ead45
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-18 11:22:14 -0500

    commit tree 393980dce161d8a5f56e673e200fb936ad6c582c

--- looping.rb
+++ looping.rb
@@ -1,28 +1,32 @@
-# def happy_new_year
-#   i = 10 
-#   while i > 0 
-#     puts i
-#     i-=1
-#   end
-#   puts "Happy New Year!"
-# end
+def happy_new_year
+  i = 10 
+  while i > 0 
+    puts i
+    i-=1
+  end
+  puts "Happy New Year!"
+end
 
-# # No need to modify this code! Use this to implement the fizzbuzz_printer method.
-# def fizzbuzz(num)
-#   if num % 3 == 0 && num % 5 == 0
-#     "FizzBuzz"
-#   elsif num % 3 == 0
-#     "Fizz"
-#   elsif num % 5 == 0
-#     "Buzz"
-#   else
-#     num
-#   end
-# end
+# No need to modify this code! Use this to implement the fizzbuzz_printer method.
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
+end
 
-# def fizzbuzz_printer
-#   # your code here
-# end
+def fizzbuzz_printer
+  (1..100).each do |i|
+    puts fizzbuzz i
+  end 
+end
+
+fizzbuzz_printer
 
 def reverse_string(str)
   chars = str.split("")
@@ -30,10 +34,7 @@ def reverse_string(str)
   reverse_chars = []
   while i >=0
     reverse_chars.push(chars[i])
-    puts reverse_chars
     i-=1
   end
-  puts reverse_chars.join('')
+  reverse_chars.join('')
 end
-
-reverse_string('hello')
